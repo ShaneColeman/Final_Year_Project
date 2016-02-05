a04c67cf81a8ed29e5b775c36235554f71bd22d7
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-02-05 16:54:40 +0000

    trainingDataSet() method renamed to trainingDataSet_8_4(). DataSet instantiated and row added to method

--- MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
+++ MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
@@ -6,6 +6,7 @@
 package musicgenreneuralnetwork;
 
 import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
 
 /**
  *
@@ -15,8 +16,11 @@ public class DataSetTrain
 {
     DataSet train;
     
-    public void trainingDataSet()
+    public void trainingDataSet_8_4()
     {
+        train = new DataSet(8,4);
         
+        train.addRow(new DataSetRow(new double[]{0.160931174, 0.486666667, 0.2, 0, 0, 0.35, 0.363636364, 0}, 
+                new double[]{0, 0, 1, 0}));
     }
 }
