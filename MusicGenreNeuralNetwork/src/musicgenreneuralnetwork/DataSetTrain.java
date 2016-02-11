e3d24ed58dcdca21cd93d821b5f771cd3f10bea4
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-02-11 14:01:38 +0000

    getInputs() method added

--- MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
+++ MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
@@ -47,6 +47,11 @@ public class DataSetTrain
         this.outputs = outputs;
     }
     
+    public int getInputs()
+    {
+        return inputs;
+    }
+    
     public DataSet getDataSet()
     {
         return train;
