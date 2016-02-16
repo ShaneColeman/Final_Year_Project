cab3f312805fab64bd5b38089f49a4ad4a1bb7c3
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-02-16 14:42:47 +0000

    setNumberOfInputs(), setNumberOfOutputs(), getNumberOfInputs(), getNumberOfOutputs() and getDataSet() methods added

--- MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetANN.java
+++ MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetANN.java
@@ -5,15 +5,30 @@
  */
 package musicgenreneuralnetwork;
 
+import org.neuroph.core.data.DataSet;
+
 /**
  *
  * @author Windows
  */
 public abstract class DataSetANN 
 {
+    private int inputs;
+    private int outputs;
+    private DataSet dataSet;
+    
     public void addTrainingDataSetRows(double[] inputs, double[] outputs){};
     
     public void addTestingDataSetRows(double[] inputs, double[] outputs){};
     
+    public void setNumberOfInputs(int inputs){this.inputs = inputs;}
     
+    public void setNumberOfOutputs(int outputs){this.outputs = outputs;}
+    
+    public int getNumberOfInputs(){return inputs;}
+    
+    public int getNumberOfOutputs(){return outputs;}
+    
+    public DataSet getDataSet(){return dataSet;}
+     
 }
