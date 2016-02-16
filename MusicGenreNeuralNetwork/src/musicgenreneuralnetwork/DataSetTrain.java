7bc563fbc0791bf0b1d70b825fe4832ff8340aa8
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-02-16 14:48:01 +0000

    DataSetANN abstract class extended in DataSetTrain class

--- MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
+++ MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/DataSetTrain.java
@@ -12,7 +12,7 @@ import org.neuroph.core.data.DataSetRow;
  *
  * @author Windows
  */
-public class DataSetTrain 
+public class DataSetTrain extends DataSetANN
 {
     private DataSet train;
     private int inputs;
@@ -35,31 +35,37 @@ public class DataSetTrain
     }
 
     
+    @Override
     public void addTrainingDataSetRows(double[] inputs, double[] outputs)
     {
         train.addRow(new DataSetRow(inputs,outputs));
     }
     
+    @Override
     public void setNumberOfInputs(int inputs)
     {
         this.inputs = inputs;
     }
     
+    @Override
     public void setNumberOfOutputs(int outputs)
     {
         this.outputs = outputs;
     }
     
+    @Override
     public int getNumberOfInputs()
     {
         return inputs;
     }
     
+    @Override
     public int getNumberOfOutputs()
     {
         return outputs;
     }
     
+    @Override
     public DataSet getDataSet()
     {
         return train;
