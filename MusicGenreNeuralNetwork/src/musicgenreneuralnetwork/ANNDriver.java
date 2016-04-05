b7b3408a164b055410f12bde1290bd2a4de4bb41
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-04-05 16:04:34 +0100

    mlp1_sig_8_6_4.nnet file saved

--- MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/ANNDriver.java
+++ MusicGenreNeuralNetwork/src/musicgenreneuralnetwork/ANNDriver.java
@@ -15,7 +15,7 @@ public class ANNDriver {
     
     public static void main(String[] args) throws FileNotFoundException 
     {
-        //ANNDriver Class Runnable 
+        //ANNDriver Class Runnable Driver
         ANNDataSetSetup aNN = new ANNDataSetSetup();
         
         //aNN.trainedMLPDataSetTrain();
